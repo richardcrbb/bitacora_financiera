79c4e36f79ae5badb14499621c888ba45a5c7b26
Cristian Beltran <ricardocrbb@hotmail.com>
2025-07-10 20:24:26 -0500

    Mejora en el grafico circular, implementacion de tooltip y efecto visual

--- lib/screens/overview_screen.dart
+++ lib/screens/overview_screen.dart
@@ -49,8 +49,10 @@ class OverviewScreenState extends State<OverviewScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Resumen de Gastos", 
-          style: TextStyle(fontWeight: FontWeight.bold)
-        ),),
+        style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -449,84 +451,154 @@ Future<Map<String, double>> obtenerGastosUltimos12Meses() async {
 // Grafico Circular 
 
 Widget construirGraficoCircular(Map<String, double> datos, BuildContext context) {
+  
+  //verifica si no hay datos para hacer el grafico
+  if (datos.isEmpty) {return Center(child: Text("No hay datos disponibles"));}
+
   final colores = [
     Colors.red, Colors.blue, Colors.green, Colors.orange,
     Colors.purple, Colors.teal, Colors.pink, Colors.brown,
   ];
 
-  int i = 0;
   double total = datos.values.fold(0, (sum, val) => sum + val);
+  
+  // Variable para controlar la visibilidad del tooltip
+  int touchedIndex =0;
+  bool showTooltip = false;
+  double currentAmount = 0;
+  Offset tooltipPosition = Offset.zero;
+  double sectionMargin = 2;
 
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      SizedBox(
-        height: 250,
-        child: PieChart(
-          PieChartData(
-            pieTouchData: PieTouchData(
-              touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                if (pieTouchResponse?.touchedSection != null) {
-                  final section = pieTouchResponse!.touchedSection!;
-                  final category = datos.keys.elementAt(section.touchedSectionIndex);
-                  final amount = datos[category]!;
-                  
-                  // Mostrar tooltip mientras se interactúa con el gráfico
-                  ScaffoldMessenger.of(context)
-                    ..removeCurrentSnackBar()
-                    ..showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          '$category: ${NumberFormat.currency(locale: 'es_CO', symbol: 'COP', decimalDigits: 0).format(amount)}',
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                        duration: const Duration(seconds: 1),
-                      ),
-                    );
-                } else {
-                  // Limpiar el tooltip cuando no se toca ninguna sección
-                  ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                }
-              },
-              enabled: true,
-            ),
-            sections: datos.entries.map((entry) {
-              final color = colores[i % colores.length];
-              final value = entry.value;
-              i++;
-              return PieChartSectionData(
-                value: value,
-                title: '${entry.key}\n${NumberFormat.compactCurrency(
-                  decimalDigits: 0,
-                  symbol: 'COP ',
-                  locale: 'es_CO'
-                ).format(value)}',
-                color: color,
-                radius: 120,
-                titleStyle: const TextStyle(
-                  fontSize: 12, 
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+  return StatefulBuilder(
+    builder: (context, setState) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 250,
+            child: Stack(
+              children: [
+                PieChart(
+                  PieChartData(
+                    sections: datos.entries.toList().asMap().entries.map((entry) {
+                      
+                      final index = entry.key;
+                      final value = entry.value.value;
+                      final cat = entry.value.key;
+                      final color = colores[index % colores.length];
+                      final bool isTouched = showTooltip && touchedIndex == index ;
+                      
+                      return PieChartSectionData(
+                        value: value,
+                        title: cat,
+                        titlePositionPercentageOffset: isTouched ? .99 : .5,
+                        color: color,
+                        radius: isTouched ? 150 : 120,
+                        titleStyle: isTouched ? 
+                        
+                          const TextStyle(
+                          fontSize: 20, 
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber,
+                          )
+                          :
+                          const TextStyle(
+                          fontSize: 12, 
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          ),
+                      );
+                    }).toList(),
+                    pieTouchData: PieTouchData(
+                      touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                          if (pieTouchResponse?.touchedSection != null && pieTouchResponse!.touchedSection!.touchedSectionIndex >= 0) {
+                          final section = pieTouchResponse.touchedSection!;
+                          final category = datos.keys.elementAt(section.touchedSectionIndex);
+                          final amount = datos[category]!;
+                          
+                          if (event is FlLongPressStart || 
+                              event is FlLongPressMoveUpdate ||
+                              event is FlPanStartEvent ||
+                              event is FlPanUpdateEvent) {
+                            setState(() {
+                              showTooltip = true;
+                              currentAmount = amount;
+                              tooltipPosition = event.localPosition!;
+                              touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
+                              sectionMargin = 10;
+                            });
+                          }
+                          
+                          if (event is FlLongPressEnd || event is FlPanEndEvent) {
+                            setState(() {
+                              showTooltip = false;
+                              sectionMargin = 2;
+                            });
+                          }
+                        } else if (event is FlLongPressEnd || event is FlPanEndEvent) {
+                          setState(() {
+                            showTooltip = false;
+                            sectionMargin = 2;
+                          });
+                        }
+                      },
+                      enabled: true,
+                      longPressDuration: const Duration(milliseconds: 100),
+                    ),
+                    centerSpaceRadius: 0,
+                    sectionsSpace: sectionMargin,
+                    startDegreeOffset: 180,
+                  ),
+                  swapAnimationDuration: Duration(milliseconds: 100),
                 ),
-              );
-            }).toList(),
-            centerSpaceRadius: 0,
-            sectionsSpace: 2,
+                if (showTooltip)
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 200),
+                    left: tooltipPosition.dx - 70,
+                    top: tooltipPosition.dy -100,
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10,
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        '${NumberFormat("#,##0", "es_COP").format(currentAmount)} pesos.',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
           ),
-        ),
-      ),
-      const SizedBox(height: 20),
-      Text(
-        "Total de gastos del mes de ${DateFormat('MMMM', 'es_CO').format(DateTime.now())}:",
-        style: const TextStyle(fontSize: 18),
-      ),
-      Text(
-        NumberFormat.currency(locale: 'es_CO', symbol: 'COP', decimalDigits: 0).format(total),
-        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
-    ],
+          const SizedBox(height: 20),
+          Text(
+            "Total de gastos del mes de ${DateFormat('MMMM', 'es_CO').format(DateTime.now())}:",
+            style: const TextStyle(fontSize: 18),
+          ),
+          Text(
+            NumberFormat.currency(locale: 'es_CO', symbol: 'COP', decimalDigits: 0).format(total),
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ],
+      );
+    },
   );
 }
+
+
+
+
 
 
 
